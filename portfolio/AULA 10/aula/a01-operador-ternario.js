016dfa1a7964257a7ba3a7a7c16af5d63b36f037
osvaldo souza <iwtraining.java@gmail.com>
2022-01-26 21:30:10 -0300

    fazendo as modificações através de uma outra branch

--- portfolio/AULA 10/aula/a01-operador-ternario.js
+++
@@ -1,8 +0,0 @@
-altura = -4.75;
-
-permitido = altura >=0 && altura>=1.22 && altura <=3;
-
-var resposta = permitido ? "pode descer no insano" : "não pode descer no insano";
-
-console.log(resposta);
-
